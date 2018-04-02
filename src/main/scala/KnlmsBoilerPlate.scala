7636baf3995c7759fb0e1d810ad5e5924d2f4165
Nick Fraser <icanlosh@gmail.com>
2018-04-02 16:40:57 +0100

    Updated maxmimum dictionary size.

--- src/main/scala/KnlmsBoilerPlate.scala
+++ src/main/scala/KnlmsBoilerPlate.scala
@@ -115,7 +115,7 @@ class ValidToDecoupledWrapper(w: Int, pipeLength: Int) extends Module {
   val queue = Module(new Queue(UInt(width=w), pipeLength)).io // A fall-through FIFO with a decoupled interface for I/O
 
   // Define the KNLMS style accelerator.
-  val n: Int = 60
+  val n: Int = 62
   val m: Int = 7
   val wL: Int = 18
   val iL: Int = 6
