2a28c79756674f852d5dc5060469f7f7647cf97c
Nick Fraser <icanlosh@gmail.com>
2018-04-08 23:29:45 +0100

    First attempt to update project scripts for zzSoC.

--- src/main/script/zzsoc.tcl
+++ src/main/script/zzsoc.tcl
@@ -1,36 +1,53 @@
-if {$argc != 5} {
-  puts "Expected: <rosetta root> <accel verilog> <proj name> <proj dir> <freq>"
-  exit
+###############################################################################
+ #  Copyright (c) 2016, Xilinx, Inc.
+ #  All rights reserved.
+ #
+ #  Redistribution and use in source and binary forms, with or without
+ #  modification, are permitted provided that the following conditions are met:
+ #
+ #  1.  Redistributions of source code must retain the above copyright notice,
+ #     this list of conditions and the following disclaimer.
+ #
+ #  2.  Redistributions in binary form must reproduce the above copyright
+ #      notice, this list of conditions and the following disclaimer in the
+ #      documentation and/or other materials provided with the distribution.
+ #
+ #  3.  Neither the name of the copyright holder nor the names of its
+ #      contributors may be used to endorse or promote products derived from
+ #      this software without specific prior written permission.
+ #
+ #  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ #  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ #  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ #  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ #  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ #  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ #  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ #  OR BUSINESS INTERRUPTION). HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ #  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ #  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ #  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ #
+###############################################################################
+###############################################################################
+ #
+ #
+ # @file pynq_revC.tcl
+ #
+ # tcl script with standard PYNQ configuration
+ #
+ #
+###############################################################################
+
+proc getPresetInfo {} {
+  return [dict create name {PYNQ} description {PYNQ}  vlnv xilinx.com:ip:processing_system7:5.5 display_name {PYNQ} ]
 }
 
-# pull cmdline variables to use during setup
-set config_rosetta_root  [lindex $argv 0]
-set config_rosetta_verilog "$config_rosetta_root/src/main/verilog"
-set config_accel_verilog [lindex $argv 1]
-set config_proj_name [lindex $argv 2]
-set config_proj_dir [lindex $argv 3]
-set config_freq [lindex $argv 4]
-puts $config_rosetta_verilog
-# fixed for platform
-set config_proj_part "xczu3eg-sbva484-1-i"
-set xdc_dir "$config_rosetta_root/src/main/script/host"
+proc validate_preset {IPINST} { return true }
 
-# set up project
-create_project $config_proj_name $config_proj_dir -part $config_proj_part
-update_ip_catalog
 
-# add the Verilog implementation for the accelerator
-add_files -norecurse $config_accel_verilog
-# add misc verilog files used by fpga-rosetta
-add_files -norecurse $config_rosetta_verilog/Q_srl.v $config_rosetta_verilog/DualPortBRAM.v
-
-# create block design
-create_bd_design "procsys"
-create_bd_cell -type ip -vlnv xilinx.com:ip:zynq_ultra_ps_e:3.1 zynq_ultra_ps_e_0
-set zups [get_bd_cells zynq_ultra_ps_e_0]
-#apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {make_external "FIXED_IO, DDR" Master "Disable" Slave "Disable" } $ps7
-#source "${xdc_dir}/pynq_revC.tcl" # Doesn't seem to require anything from here.
-set_property -dict [ list \
+proc apply_preset {IPINST} {
+return [dict create \
     CONFIG.PSU_BANK_0_IO_STANDARD {LVCMOS18} \
     CONFIG.PSU_BANK_1_IO_STANDARD {LVCMOS18} \
     CONFIG.PSU_BANK_2_IO_STANDARD {LVCMOS18} \
@@ -315,75 +332,7 @@ set_property -dict [ list \
     CONFIG.PSU__USB3_1__PERIPHERAL__IO {GT Lane3} \
     CONFIG.PSU__USE__IRQ0 {1} \
     CONFIG.PSU__USE__M_AXI_GP2 {1} \
-] $zups
-
-#THIS set_property -dict [apply_preset $ps7] $ps7
-# enable AXI HP ports, set target frequency
-set_property -dict [list CONFIG.PSU__USE__S_AXI_GP2 {1} CONFIG.PSU__USE__S_AXI_GP3 {1} CONFIG.PSU__USE__S_AXI_GP4 {1} CONFIG.PSU__USE__S_AXI_GP5 {1}] $zups
-#THIS set_property -dict [list CONFIG.PCW_USE_S_AXI_HP0 {1} CONFIG.PCW_USE_S_AXI_HP1 {1} CONFIG.PCW_USE_S_AXI_HP2 {1} CONFIG.PCW_USE_S_AXI_HP3 {1}] $ps7
-# TODO expose top-level ports?
-#THIS #set_property -dict [list CONFIG.PCW_FPGA0_PERIPHERAL_FREQMHZ $config_freq CONFIG.PCW_USE_S_AXI_HP0 {1} CONFIG.PCW_USE_S_AXI_HP1 {1} CONFIG.PCW_USE_S_AXI_HP2 {1} CONFIG.PCW_USE_S_AXI_HP3 {1}] [get_bd_cells processing_system7_0]
-#THIS set_property -dict [list CONFIG.PCW_FPGA0_PERIPHERAL_FREQMHZ $config_freq CONFIG.PCW_FPGA1_PERIPHERAL_FREQMHZ {142.86} CONFIG.PCW_FPGA2_PERIPHERAL_FREQMHZ {200} CONFIG.PCW_FPGA3_PERIPHERAL_FREQMHZ {166.67} CONFIG.PCW_EN_CLK1_PORT {1} CONFIG.PCW_EN_CLK2_PORT {1} CONFIG.PCW_EN_CLK3_PORT {1} CONFIG.PCW_USE_M_AXI_GP0 {1}] $ps7
-set_property -dict [list CONFIG.PSU__FPGA_PL0_ENABLE {1} CONFIG.PSU__FPGA_PL1_ENABLE {1} CONFIG.PSU__FPGA_PL2_ENABLE {1} CONFIG.PSU__FPGA_PL3_ENABLE {1}] $zups
-set_property -dict [list CONFIG.PSU__CRL_APB__PL0_REF_CTRL__FREQMHZ $config_freq CONFIG.PSU__CRL_APB__PL1_REF_CTRL__FREQMHZ {142.86} CONFIG.PSU__CRL_APB__PL2_REF_CTRL__FREQMHZ {200} CONFIG.PSU__CRL_APB__PL3_REF_CTRL__FREQMHZ {166.67}] $zups
-
-# add the accelerator RTL module into the block design
-create_bd_cell -type module -reference PYNQWrapper PYNQWrapper_0
-
-# connect control-status registers
-#THIS apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/ps7/M_AXI_GP0" Clk "Auto" }  [get_bd_intf_pins PYNQWrapper_0/csr]
-apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/zynq_ultra_ps_e_0/M_AXI_HPM0_LPD" Clk "Auto" }  [get_bd_intf_pins PYNQWrapper_0/csr]
-#apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/zynq_ultra_ps_e_0/M_AXI_HPM0_LPD" intc_ip "New AXI Interconnect" Clk_xbar "Auto" Clk_master "Auto" Clk_slave "Auto" }  [get_bd_intf_pins PYNQWrapper_0/csr]
-
-# connect AXI master ports
-apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/PYNQWrapper_0/mem0" Clk "Auto" }  [get_bd_intf_pins zynq_ultra_ps_e_0/S_AXI_HP0_FPD]
-apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/PYNQWrapper_0/mem1" Clk "Auto" }  [get_bd_intf_pins zynq_ultra_ps_e_0/S_AXI_HP1_FPD]
-apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/PYNQWrapper_0/mem2" Clk "Auto" }  [get_bd_intf_pins zynq_ultra_ps_e_0/S_AXI_HP2_FPD]
-apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/PYNQWrapper_0/mem3" Clk "Auto" }  [get_bd_intf_pins zynq_ultra_ps_e_0/S_AXI_HP3_FPD]
-#apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/PYNQWrapper_0/mem0" intc_ip "Auto" Clk_xbar "Auto" Clk_master "Auto" Clk_slave "Auto" }  [get_bd_intf_pins zynq_ultra_ps_e_0/S_AXI_HP0_FPD]
-#apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/PYNQWrapper_0/mem1" intc_ip "Auto" Clk_xbar "Auto" Clk_master "Auto" Clk_slave "Auto" }  [get_bd_intf_pins zynq_ultra_ps_e_0/S_AXI_HP1_FPD]
-#apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/PYNQWrapper_0/mem2" intc_ip "Auto" Clk_xbar "Auto" Clk_master "Auto" Clk_slave "Auto" }  [get_bd_intf_pins zynq_ultra_ps_e_0/S_AXI_HP2_FPD]
-#apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/PYNQWrapper_0/mem3" intc_ip "Auto" Clk_xbar "Auto" Clk_master "Auto" Clk_slave "Auto" }  [get_bd_intf_pins zynq_ultra_ps_e_0/S_AXI_HP3_FPD]
-
-# rewire reset port to use active-high
-disconnect_bd_net [get_bd_nets rst_ps8*peripheral_aresetn] [get_bd_pins PYNQWrapper_0/reset]
-connect_bd_net [get_bd_pins [get_bd_cells *rst_ps8*]/peripheral_reset] [get_bd_pins PYNQWrapper_0/reset]
-
-# connect accelerator AXI masters to Zynq PS
-#apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/PYNQWrapper_0/mem0" Clk "Auto" }  [get_bd_intf_pins processing_system7_0/S_AXI_HP0]
-#apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/PYNQWrapper_0/mem1" Clk "Auto" }  [get_bd_intf_pins processing_system7_0/S_AXI_HP1]
-#apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/PYNQWrapper_0/mem2" Clk "Auto" }  [get_bd_intf_pins processing_system7_0/S_AXI_HP2]
-#apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/PYNQWrapper_0/mem3" Clk "Auto" }  [get_bd_intf_pins processing_system7_0/S_AXI_HP3]
-# make the block design look prettier
-regenerate_bd_layout
-validate_bd_design
-save_bd_design
-# write block design tcl
-write_bd_tcl $config_proj_dir/rosetta.tcl
-
-# use global mode (no out-of-context) for bd synthesis
-#set_property synth_checkpoint_mode None [get_files $config_proj_dir/$config_proj_name.srcs/sources_1/bd/procsys/procsys.bd]
-
-# create HDL wrapper
-make_wrapper -files [get_files $config_proj_dir/$config_proj_name.srcs/sources_1/bd/procsys/procsys.bd] -top
-add_files -norecurse $config_proj_dir/$config_proj_name.srcs/sources_1/bd/procsys/hdl/procsys_wrapper.v
-update_compile_order -fileset sources_1
-update_compile_order -fileset sim_1
-set_property top procsys_wrapper [current_fileset]
-
-# use manual compile order to ensure accel verilog is processed prior to block design
-#update_compile_order -fileset sources_1
-#set_property source_mgmt_mode DisplayOnly [current_project]
+]
+}
 
 
-# set synthesis strategy
-set_property strategy Flow_PerfOptimized_high [get_runs synth_1]
-
-set_property STEPS.SYNTH_DESIGN.ARGS.DIRECTIVE AlternateRoutability [get_runs synth_1]
-set_property STEPS.SYNTH_DESIGN.ARGS.RETIMING true [get_runs synth_1]
-
-set_property strategy Performance_ExtraTimingOpt [get_runs impl_1]
-set_property STEPS.OPT_DESIGN.ARGS.DIRECTIVE Explore [get_runs impl_1]
-set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.ARGS.DIRECTIVE AggressiveExplore [get_runs impl_1]
-set_property STEPS.PHYS_OPT_DESIGN.ARGS.DIRECTIVE AggressiveExplore [get_runs impl_1]
-set_property STEPS.POST_ROUTE_PHYS_OPT_DESIGN.IS_ENABLED true [get_runs impl_1]
